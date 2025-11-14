e48cdda3af7497354d12a3ecc8cea620c4c8b37f
schizo <schizo@debian.debian>
2025-11-14 01:10:02 +0000

    Added base movie card component

---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,28 @@
+
+function MovieCard({movie}){
+
+    function onFavourite(){
+        alert("Clicked")
+    }
+
+    return(
+        <>
+            <div className="movie-card">
+                <div className="movie-poster">
+                    <img src={movie.url} alt={movie.title}></img>
+                    <div className="movie-overlay">
+                        <button className="favourite-btn" onClick={onFavourite}>
+                            ♥︎
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.date}</p>
+            </div>
+        </>
+    );
+}
+
+export default MovieCard
